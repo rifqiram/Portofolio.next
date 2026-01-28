6777496c2c49e0ce2729f4544e12f86e94e72091
Rifqi Ramadhan <rifqiram6140@gmail.com>
2026-01-28 15:02:07 +0700

    absen sore

--- app/page.js
+++ app/page.js
@@ -7,7 +7,6 @@ import CertificateSection from "./components/CertificateSection";
 import ContactSection from "./components/ContactSection";
 import Footer from "./components/Footer";
 
-// test
 export default function Home() {
   return (
     <main className="bg-[#0a0a0a]">
