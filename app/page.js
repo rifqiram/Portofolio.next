06416b2b757a706b86957d7e6b63a93925bd1d23
Rifqi Ramadhan <rifqiram6140@gmail.com>
2026-02-12 14:51:34 +0700

    hadir

--- app/page.js
+++ app/page.js
@@ -8,7 +8,7 @@ import ContactSection from "./components/ContactSection";
 import Footer from "./components/Footer";
 
 
-// Main Page 
+// Main Page hadir om
 
 export default function Home() {
   return (
