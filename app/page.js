85acc7616fa2b9635263e07853354c746e592452
Rifqi Ramadhan <rifqiram6140@gmail.com>
2026-02-15 12:24:44 +0700

    update minggu

--- app/page.js
+++ app/page.js
@@ -8,7 +8,7 @@ import ContactSection from "./components/ContactSection";
 import Footer from "./components/Footer";
 
 
-// Main Page liburan update
+// Main Page 
 
 export default function Home() {
   return (
